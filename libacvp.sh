d9450d44d79b7e4af0ca130bbbdfb1e998c61c4f
pandrey76 <pandrey76@yandex.ru>
2020-11-24 17:49:18 +0300

    Edit libacvp.sh file.
    Add libacvp source files to archive.

--- libacvp.sh
+++ libacvp.sh
@@ -19,57 +19,82 @@ SOURCE_FOLDER_PROJECT="${ACVP_PROJECT_FOLDER}/${SOURCE_FOLDER_NAME}"
 ARCHIVE_FOLDER_PROJECT="${ACVP_PROJECT_FOLDER}/${ARCHIVE_FOLDER_NAME}"
 
 
+# OpenSSL project
+########################################################################
+
 OPENSSL_INSTALL_FOLDER="${INSTALL_FOLDER_PROJECT}/openssl-1.1.1b_install"
-CURL_INSTALL_FOLDER="${INSTALL_FOLDER_PROJECT}/curl-7.64.1_install"
-LIBACVP_INSTALL_FOLDER="${INSTALL_FOLDER_PROJECT}/libacvp_install"
 
 # wget https://www.openssl.org/source/openssl-1.1.1b.tar.gz && tar -xf openssl-1.1.1b.tar.gz -C "${SOURCE_FOLDER_PROJECT}"
 
 # --no-check-certificate
 wget --no-check-certificate https://www.openssl.org/source/openssl-1.1.1b.tar.gz && tar -xf openssl-1.1.1b.tar.gz -C "${SOURCE_FOLDER_PROJECT}"
-
 mv -f openssl-1.1.1b.tar.gz "${ARCHIVE_FOLDER_PROJECT}"
+
 export OPENSSL_INSTALL="${OPENSSL_INSTALL_FOLDER}"
 # Adding debug build ( ./config -d )
 cd "${SOURCE_FOLDER_PROJECT}/openssl-1.1.1b" && ./config shared -d --prefix="${OPENSSL_INSTALL}" && make clean && make && make install
-#
-## Install Curl for network transport
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${OPENSSL_INSTALL}/lib"
+
+########################################################################
+
+# Curl project
+########################################################################
+
+CURL_INSTALL_FOLDER="${INSTALL_FOLDER_PROJECT}/curl-7.64.1_install"
 export CURL_INSTALL="${CURL_INSTALL_FOLDER}"
 # wget https://curl.haxx.se/download/curl-7.64.1.tar.gz && tar -xf curl-7.64.1.tar.gz -C "${SOURCE_FOLDER_PROJECT}"
 
 # --no-check-certificate
 wget --no-check-certificate https://curl.haxx.se/download/curl-7.64.1.tar.gz && tar -xf curl-7.64.1.tar.gz -C "${SOURCE_FOLDER_PROJECT}"
-
 mv -f curl-7.64.1.tar.gz "${ARCHIVE_FOLDER_PROJECT}"
 ##
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${OPENSSL_INSTALL}/lib"
+
 # Adding debug build ( ./configure --enable-debug )
 cd "${SOURCE_FOLDER_PROJECT}/curl-7.64.1" && ./configure --enable-debug --prefix="${CURL_INSTALL}" --with-ssl="${OPENSSL_INSTALL}" && make && make install
-##
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CURL_INSTALL/lib}"
+
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CURL_INSTALL}/lib"
+
+########################################################################
 
 ##
 cd "${SOURCE_FOLDER_PROJECT}"
-#
-git clone "https://github.com/cisco/libacvp.git"
+##
 
+# libacvp Project
+########################################################################
 
-cd "libacvp"
+LIB_ACVP_NAME="libacvp"
+LIBACVP_INSTALL_FOLDER="${INSTALL_FOLDER_PROJECT}/${LIB_ACVP_NAME}_install"
 
-## Convert new-line symbol to Unix format
+# Clone repository libacvp
+git clone "https://github.com/cisco/${LIB_ACVP_NAME}.git"
+
+# Archive libacvp source project
+tar -czvf ${LIB_ACVP_NAME}.tar.gz "${LIB_ACVP_NAME}"
+mv -f ${LIB_ACVP_NAME}.tar.gz "${ARCHIVE_FOLDER_PROJECT}"
+
+cd "${LIB_ACVP_NAME}"
+
+# Convert new-line symbol to Unix format
 find . -type f -print0 | xargs -0 dos2unix
 
-##
+# Make file configure executable.
+chmod a+x configure
+
 # Adding debug build ( CFLAGS="-O0 -g" ./configure )
 CFLAGS="-O0 -g" ./configure --with-ssl-dir="$OPENSSL_INSTALL" --with-libcurl-dir="$CURL_INSTALL" --prefix="${LIBACVP_INSTALL_FOLDER}" && make && make install
-#
-export ACV_SERVER="demo.acvts.nist.gov"
-export ACV_PORT="443"
-export ACV_URI_PREFIX="acvp/v1/"
-export ACV_API_CONTEXT="acvp/"
-#
-cd "${LIBACVP_INSTALL_FOLDER}"
-#./app/acvp_app --help
-#./app/acvp_app --all_algs
 
-./bin/acvp_app --all_algs
+########################################################################
+
+# Sample for executing
+
+# export ACV_SERVER="demo.acvts.nist.gov"
+# export ACV_PORT="443"
+# export ACV_URI_PREFIX="acvp/v1/"
+# export ACV_API_CONTEXT="acvp/"
+# #
+# cd "${LIBACVP_INSTALL_FOLDER}"
+# #./app/acvp_app --help
+# #./app/acvp_app --all_algs
+#
+# ./bin/acvp_app --all_algs
