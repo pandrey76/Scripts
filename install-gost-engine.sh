9ff80ce965caf65561da4ac8f1c9008a5cba172d
pandrey76 <pandrey76@yandex.ru>
2022-05-22 12:25:05 +0300

    Edited install-gost-engine.sh file.
    Added correcting process function returning.

--- install-gost-engine.sh
+++ install-gost-engine.sh
@@ -13,9 +13,9 @@ function is_correct_sha256() {
         echo "The file path that containing crc: ${PATH_TO_SHA256_FILE} doesn't exist."
         return 1
     fi
-    CHECKING_FILE_NAME=${PATH_TO_SOURCE_FILE##*/}
+    local CHECKING_FILE_NAME=${PATH_TO_SOURCE_FILE##*/}
     echo "The calculating fle name is ${CHECKING_FILE_NAME}"
-    CURRENT_CRC_LINE=$(sha256sum ${PATH_TO_SOURCE_FILE})
+    local CURRENT_CRC_LINE=$(sha256sum "${PATH_TO_SOURCE_FILE}")
     echo "Output from sha256sum work: ${CURRENT_CRC_LINE}"
     
     # read -a strarr <<< "Şstring"
@@ -27,10 +27,10 @@ function is_correct_sha256() {
 
     while IFS=' ' read -r CRC FILE_NAME
       do
-        printf 'CRC: %s, FILE: %s\n' "${CRC}" "${FILE_NAME}"
+        # printf 'CRC: %s, FILE: %s\n' "${CRC}" "${FILE_NAME}"
         if [[ "${PATH_TO_SOURCE_FILE##*/}" == "${FILE_NAME}" ]];
          then
-           echo "Find calculating file name: ${FILE_NAME}"
+           printf 'CRC: %s, FILE: %s\n' "${CRC}" "${FILE_NAME}"
            if [[ ${CURRENT_CRC} == ${CRC} ]];
              then
                printf 'CRC of file: %s is correct!\n' "${FILE_NAME}"
@@ -41,7 +41,7 @@ function is_correct_sha256() {
            fi   
         fi
       done <"$PATH_TO_SHA256_FILE"
-
+      printf "Error comparing file name of calculating crc file with file name containing in crc file."
       return 1
 }
 
@@ -121,13 +121,13 @@ CMAKE_LATEST_RELEASE_SHA256_FILE_URL="${CMAKE_LATEST_RELEASE_URL}${CMAKE_LATEST_
 # wget --no-check-certificate "${CMAKE_LATEST_RELEASE_SHA256_FILE_URL}"
 
 is_correct_sha256 "./${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}" "./${CMAKE_LATEST_RELEASE_SHA256_FILE_NAME}"
+if [[ $? -eq 1 ]]
+  then
+   echo "The file '${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}' don't match to CRC."
+fi
 
-#if [[ ! is_correct_sha256 "${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}" "./${CMAKE_LATEST_RELEASE_SHA256_FILE_NAME}" ]];
-#  then      
-#   echo "The file ${CMAKE_LATEST_RELEASE_TARGZ_FILE} dont match to CRC."
-#fi
 
-tar -xf "./${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}" -C "${SOURCE_FOLDER_PROJECT}"
+# tar -xf "./${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}" -C "${SOURCE_FOLDER_PROJECT}"
 # mv -f "./${CMAKE_LATEST_RELEASE_TARGZ_FILE_NAME}" "${ARCHIVE_FOLDER_PROJECT}"
 
 # mv -f "./${CMAKE_LATEST_RELEASE_SHA256_FILE_NAME}" "${ARCHIVE_FOLDER_PROJECT}"
